a60cdb40e80c8d1072f08a29120ce8999ea9f12e
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-11-05 14:06:01 +0400

    feat: isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -317,8 +317,9 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const newStr = str.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  return newStr === newStr.split('').reverse().join('');
 }
 
 /**
