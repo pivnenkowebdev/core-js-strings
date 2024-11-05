0703962ef01ad78e55053d97c124664a518d8696
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-11-05 15:32:08 +0400

    feat: add sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -187,8 +187,14 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let result = 0;
+  if (str && str.length > 0) {
+    for (let i = 0; i < str.length; i += 1) {
+      result += str.charCodeAt(i);
+    }
+  }
+  return result;
 }
 
 /**
